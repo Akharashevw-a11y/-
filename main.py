03b596308d94949a8cf72082b8bf5f9d5b40ae61
Мага <Akharashevw@mail.ru>
2026-07-26 20:39:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-impor# ВЕРСИЯ 2
+import os
+
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+
 from handlers import stock, add
 
 
@@ -14,7 +16,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Команды:\n"
         "/stock — посмотреть склад\n"
         "/add — добавить шины"
-    
+    )
 
 
 app = Application.builder().token(TOKEN).build()
@@ -22,6 +24,5 @@ app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("stock", stock))
 app.add_handler(CommandHandler("add", add))
-
 
 app.run_polling()
